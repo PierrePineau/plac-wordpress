e85c055d3ddf9a51e36fd14f93f7f3b5755097a0
Leo Teixeira <leo.teixeira@diggers-consulting.com>
2024-10-16 13:46:52 +0200

    icon

--- icons.cjs
+++ icons.cjs
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const fantasticon = require('fantasticon');
+const fs = require("fs");
+const path = require("path");
+const fantasticon = require("fantasticon");
 
 // Résolution correcte des chemins sous Windows
-const inputDir = path.resolve(__dirname, 'assets/svg');
-const outputDir = path.resolve(__dirname, 'assets/icons');
+const inputDir = path.resolve(__dirname, "assets/svg");
+const outputDir = path.resolve(__dirname, "assets/icons");
 
 // Vérifier que le dossier SVG existe et contient des fichiers
 if (!fs.existsSync(inputDir) || fs.readdirSync(inputDir).length === 0) {
-  console.error('Le dossier assets/svg est vide ou n\'existe pas');
+  console.error("Le dossier assets/svg est vide ou n'existe pas");
   process.exit(1);
 }
 
@@ -16,20 +16,23 @@ if (!fs.existsSync(inputDir) || fs.readdirSync(inputDir).length === 0) {
 const config = {
   inputDir: inputDir,
   outputDir: outputDir,
-  fontTypes: ['ttf', 'woff', 'woff2'],
-  assetTypes: ['css'],
-  name: 'icon',
-  prefix: 'icon',
-  selector: '.icon',
-  fontsUrl: '../fonts',
+  fontTypes: ["ttf", "woff", "woff2"],
+  assetTypes: ["css"],
+  name: "icon",
+  prefix: "icon",
+  selector: ".icon",
+  fontsUrl: "./assets/icons",
   pathOptions: {
-    css: path.resolve(__dirname, 'dist/css/my-icon-font.css'),
+    css: path.resolve(__dirname, "assets/styles/icon.css"),
   },
 };
 
 // Exécuter Fantasticon pour générer les icônes
-fantasticon.generateFonts(config).then(() => {
-  console.log('Police d\'icônes générée avec succès');
-}).catch((error) => {
-  console.error('Erreur lors de la génération des icônes:', error);
-});
+fantasticon
+  .generateFonts(config)
+  .then(() => {
+    console.log("Police d'icônes générée avec succès");
+  })
+  .catch((error) => {
+    console.error("Erreur lors de la génération des icônes:", error);
+  });
