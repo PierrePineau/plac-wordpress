f47be368297b5abcc5d8c7ca277038005e37f719
PierrePineau <pierrepineau.pro@gmail.com>
2024-11-25 16:03:44 +0100

    c

--- assets/js/global.js
+++ assets/js/global.js
@@ -21,12 +21,12 @@ if (sliderFeatures) {
     sliderFeatures.__instance = slider;
 }
 
-const headerContainer = document.querySelector('.site-header');
+const siteheaders = document.querySelector('.site-header');
 // On regearde si on est en haut de la page, si oui on ajoute une classe sinon
 window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
-        headerContainer.classList.add('scrolled');
+        siteheaders.classList.add('scrolled');
     } else {
-        headerContainer.classList.remove('scrolled');
+        siteheaders.classList.remove('scrolled');
     }
 });
